ca8a38f9d411da2066be13183b6f354e42d491eb
Epigram <nikitakruspe@gmail.com>
2022-03-27 19:52:04 +0300

    Новый вид скрещивания

--- gen/crossoverOperators.py
+++ gen/crossoverOperators.py
@@ -29,6 +29,33 @@ class CrossoverOperators(object):
             return selected
 
     @staticmethod
+    def __blend_crossover(selected_individuals, alpha=0.1):
+        """
+        Скрещивание смешиванием
+        :param selected_individuals: отобранные на этапе селекции особи
+        :param probability: вероятность скрещивания
+        :return: результат скрещивания (список особей)
+        """
+        if len(selected_individuals) != 0:
+            psf_size = selected_individuals[0].psf.shape[0]
+            selected = selected_individuals[:]
+
+            for parent1, parent2 in zip(selected[::2], selected[1::2]):
+                psf1 = parent1.psf.flatten()
+                psf2 = parent2.psf.flatten()
+
+                size = len(psf1)
+                for i in range(size):
+                    gamma = (1. + 2. * alpha) * random.random() - alpha
+                    psf1[i] = (1. - gamma) * psf1[i] + gamma * psf2[i]
+                    psf2[i] = gamma * psf1[i] + (1. - gamma) * psf2[i]
+
+                parent1.psf = np.reshape(psf1, (psf_size, psf_size))
+                parent2.psf = np.reshape(psf2, (psf_size, psf_size))
+
+            return selected
+
+    @staticmethod
     def crossover(selected_individuals, crossover_args):
         try:
             type = crossover_args["type"]
@@ -40,3 +67,5 @@ class CrossoverOperators(object):
 
         if type == "uniform":
             return CrossoverOperators.__uniform_crossover(selected_individuals, probability)
+        elif type == "blend":
+            return CrossoverOperators.__blend_crossover(selected_individuals)
