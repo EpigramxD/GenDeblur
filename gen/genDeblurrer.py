082e0c3066b85e064dc16c47c070c6a754cf5b3c
Epigram <nikitakruspe@gmail.com>
2022-04-01 18:06:33 +0300

    Эксперимент с коэффициентом. Закомментированное увеличение вероятности мутации с ростом количества популяций.

--- gen/genDeblurrer.py
+++ gen/genDeblurrer.py
@@ -20,7 +20,7 @@ def fit_range(blurred, deconv_type, no_ref_metric_type, population_range, empty_
         deblurred_image = ImgDeconv.do_deconv(blurred, individual.psf, deconv_type)
         # individual.score = ImgQuality.get_no_ref_quality(deblurred_image, no_ref_metric_type) + ImgQuality.get_ref_quality(self.__sharp, deblurred_image, ref_metric_type)
         # individual.score = ImgQuality.frob_metric_simple(deblurred_image, self.__blurred, individual.psf) + 100000 * ImgQuality.get_no_ref_quality(deblurred_image, no_ref_metric_type)
-        individual.score = ImgQuality.test_map_metric(deblurred_image, blurred) + 15000 * ImgQuality.get_no_ref_quality(deblurred_image, no_ref_metric_type)
+        individual.score = ImgQuality.test_map_metric(deblurred_image, blurred) + 1000 * individual.psf_size * ImgQuality.get_no_ref_quality(deblurred_image, no_ref_metric_type)
         #individual.score = ImgQuality.frob_metric2(deblurred_image, blurred, individual.psf) + ImgQuality.get_no_ref_quality(deblurred_image, no_ref_metric_type)
         # individual.score = get_no_ref_quality(deblurred_image, self.no_ref_metric) #+ get_ref_qualiy(self.__sharp, deblurred_image, self.ref_metric)
         empty_list.append(copy.deepcopy(individual))
@@ -95,8 +95,11 @@ class GenDeblurrer(object):
         best_quality_in_pop = -10000000000.0
         upscale_flag = 0
         best_kernels = []
+        mutation_pos_prob_start = self.__mutation_args[0]["pos_probability"]
 
         for i in range(0, len(scale_pyramid.psf_sizes), 1):
+            self.__mutation_args[0]["pos_probability"] = mutation_pos_prob_start
+            pop_count = 0
             # считаем, что выгоднее использовать по времени
             cpu_count = mp.cpu_count()
 
@@ -153,7 +156,7 @@ class GenDeblurrer(object):
                 crossed_individuals = CrossoverOperators.crossover(selected_individuals, self.__crossover_args[0])
                 # мутация
                 # с ростом разрешения ядра вероятность увеличения белых пикселей растет
-                self.__mutation_args[0]["pos_probability"] = self.__population.current_psf_size / 30
+                # self.__mutation_args[0]["pos_probability"] += pop_count / 1000
                 mutated_individuals = MutationOperators.mutate(crossed_individuals, self.__mutation_args[0])
                 # TODO: рассмотреть вариант с динамической положительной вероятностью мутации
                 # self.__mutation_args[0]["pos_probability"] = 2.7 * 1 / scale_pyramid.psf_sizes[i]
@@ -163,6 +166,7 @@ class GenDeblurrer(object):
                 self.__population.individuals.extend(copy.deepcopy(mutated_individuals))
                 self.__population.individuals.extend(copy.deepcopy(elite_individuals))
                 upscale_flag += 1
+                pop_count += 1
 
             # апскейлим
             if i != len(scale_pyramid.psf_sizes) - 1:
